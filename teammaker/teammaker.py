0315a042e30891d56f0f7e5f8657464d79a5af99
glautz <68159838+glautz@users.noreply.github.com>
2024-12-27 20:22:52 +0100

    Create teammaker.py

---
+++ teammaker/teammaker.py
@@ -0,0 +1,13 @@
+from redbot.core import commands
+
+class teamMaker(commands.Cog):
+    """teammaker cog"""
+
+    def __init__(self, bot):
+        self.bot = bot
+
+    @commands.command()
+    async def shuffle(self, ctx):
+        """This does stuff!"""
+        # Your code will go here
+        await ctx.send("I can do stuff!")
