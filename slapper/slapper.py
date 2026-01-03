edd48ee871d64c8b6920f0da512194eb28167c29
glautz <glautz@hotmail.com>
2026-01-03 17:05:29 +0100

    Fix again...

--- slapper/slapper.py
+++ slapper/slapper.py
@@ -2,20 +2,17 @@ from redbot.core import commands
 from redbot.core.bot import Red
 import discord
 
-class SlapPer(commands.Cog):
+class Slapper(commands.Cog):
     """Slap command cog."""
 
     def __init__(self, bot: Red):
         self.bot = bot
 
-    @commands.command()
-    async def load(self, ctx):
-        """Dummy command to load slash commands (ignore)."""
-        pass
-
     @commands.hybrid_command(name="slap", description="Slap someone with a large trout.")
     async def slap(self, ctx: commands.Context, user: discord.Member):
-        """Slap a user with a large trout."""
         invoker = ctx.author.mention
         target = user.mention
         await ctx.send(f"{invoker} slaps {target} with a large trout!")
+
+async def setup(bot: Red):
+    await bot.add_cog(Slapper(bot))
